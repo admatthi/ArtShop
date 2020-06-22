3f94e2dd2070bd13e386030157d06453097c0d92
Alek <alek.matthiessen@gmail.com>
2020-06-21 20:57:16 -0700

    Initial Commit

---
+++ Bottles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bottles
+//
+//  Created by Alek Matthiessen on 6/21/20.
+//  Copyright © 2020 The Matthiessen Group, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
